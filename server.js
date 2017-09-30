3cd582bb47b16a82377a32a966ac91f4f8989828
Ajmwagar <ajmw.subs@gmail.com>
2017-09-30 16:47:56 -0700

    Added node structer

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+
+const path = require('path');
+
+
+app.use('/public', express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './', 'index.html'));
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!')
+})
+
